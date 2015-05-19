fdcf5cc14f2166806cdc3c6facec911c13784ebd
Will Barton <william.barton@cfpb.gov>
2015-05-19 14:20:26 -0400

    Add send_to.py and requirements and update the README

--- README.md
+++ README.md
@@ -18,6 +18,87 @@ To get the rest of the eRegulations stack up and working, please see the
 [regulations-bootstrap](https://github.com/cfpb/regulations-bootstrap)
 repository.
 
+## Requirements
+
+Requirements for the [`send_to.py`](send_to.py) script can be satisfied with `pip`:
+
+```shell
+$ pip install -r requirements.txt
+```
+
+- [Requests](http://docs.python-requests.org/en/latest/) for accessing
+  the [regulations-core](https://github.com/cfpb/regulations-core) API
+- [boto](https://boto.readthedocs.org/en/latest/) for Amazon S3 support
+
+## Usage
+
+This repository includes a [`send_to.py`](send_to.py) script which can
+send  the JSON in the [`stub`](stub) folder to either a running instance 
+of [regulations-core](https://github.com/cfpb/regulations-core) or to an
+Amazon S3 bucket. It can send either all of the JSON for a particular
+regulation or a single JSON file.
+
+`send_to.py` requires one of the following options that specify where
+the JSON should be sent:
+
+* `-a`, `--api-base`: The regulations-core API URL, used to 
+  [send a regulation to regulations-core](#sending-a-regulation-to-regulations-core)
+* `-b`, `--s3-bucket`: An S3 bucket name, used to 
+  [send a regulation to S3](#sending-a-regulation-to-s3)
+
+It requires one of the following options that specify what JSON should
+be sent:
+
+* `-r`, `--regulation`: The specific regulation part number to upload (eg. 1026).
+* `-f`, `--files`: Specific JSON files to upload.
+
+If you want to send JSON that does not live in the same directory as the
+`send_to.py` script you can specify that:
+
+* `-s`, `--stub-base`: The base filesystem path for the JSON to be sent (default: ./stub).
+
+### Sending a regulation to regulations-core
+
+To send a particular regulation to a running regulations-core instance,
+you can use the `send_to.py` from the root of regulations-stub like so:
+
+```shell
+./send_to.py -a http://localhost:7000 -r 1005
+```
+
+This will look in the [`stub/`](stub) subfolder of regulations-stub for
+all JSON files related to the regulation with part number 1005 (CFPB
+Regulation E) and upload them to regulations-core running at
+http://localhost:7000.
+
+### Sending a regulation to S3
+
+To send a particular regulation to an Amazon S3 bucket you will need to
+create the S3 bucket and then set the environment variables 
+`AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY` with the appropriate
+accesss credentials. Then you can use the `send_to.py` from the root 
+of regulations-stub like so:
+
+```shell
+./send_to.py -b a-regulations-bucket -r 1005
+```
+
+This will look in the [`stub/`](stub) subfolder of regulations-stub for
+all JSON files related to the regulation with part number 1005 (CFPB
+Regulation E) and upload them to the S3 bucket named
+`a-regulations-bucket`.
+
+### Sending specific JSON files
+
+To send a specific JSON file or files, you can use `send_to.py` like so:
+
+```shell
+./send_to.py -a http://localhost:7000 -f regulation/1005/2011-31725
+```
+
+This will send the file `regulation/1005/2011-31725` in the [`stub/`](stub) 
+subfolder of regulations-stub to the regulations-core API running at 
+http://localhost:7000.
 
 ## Open source licensing info
 1. [TERMS](TERMS.md)
---
+++ send_to.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import requests
+import sys
+import os
+import argparse
+import json
+import urlparse
+import boto
+
+
+def find_regulation_files(stub_base, regulation):
+    """
+    Find all JSON files in the `stub_base` belonging to the given
+    `regulation`. Returns a list of paths relative to stub_base.
+    """
+
+    # regulations-parser outputs JSON files in the following directory
+    # structure:
+    #   regulation/
+    #       [regulation part number]/
+    #           [notice number]
+    #           ...
+    #   notice/
+    #       [notice number]
+    #       ...
+    #   layer/
+    #       [layer name]/
+    #           [regulation part number]/
+    #               [notice number]
+    #               ...
+    #   diff/
+    #       [regulation part number]/
+    #           [notice number]/
+    #               [notice number]
+    #               ...
+    # 
+
+    regulation_files = []
+    notice_names = None
+
+    # Get the regulation/ JSON and notice numbers
+    print("getting files for regulation {}...".format(regulation))
+    regulation_base = os.path.join(stub_base, 'regulation', regulation)
+    if not os.path.isdir(regulation_base):
+        print("ERROR: Can't find regulation JSON for {} at {}".format(regulation, regulation_base), 
+                file=sys.stderr)
+        return []
+    for dirname, subdirs, files in os.walk(regulation_base):
+        notice_names = files
+        regulation_files.extend([os.path.join(dirname, f) for f in files])
+
+    # Get notice JSON
+    print("getting notice files for regulation {}...".format(regulation))
+    for dirname, subdirs, files in os.walk(os.path.join(stub_base, 'notice')):
+        # Notices are not stored in a regulation-part-number
+        # subdirectory. Use notice_names, from above, to just grab the
+        # ones we want.
+        notice_files = [os.path.join(dirname, f) for f in files if f in notice_names]
+        regulation_files.extend(notice_files)
+
+    # Get layer JSON
+    print("getting layer files for regulation {}...".format(regulation))
+    for dirname, subdirs, files in os.walk(os.path.join(stub_base, 'layer')):
+        # For layers, dig into each subdirectory of the layer path until
+        # we find one with our regulation part number.
+        if dirname.endswith(regulation):
+            layer_files = [os.path.join(dirname, f) for f in files if f in notice_names]
+            regulation_files.extend(layer_files)
+
+    # Get diff JSON
+    print("getting diff files for regulation {}...".format(regulation))
+    for dirname, subdirs, files in os.walk(os.path.join(stub_base, 'diff', regulation)):
+        # For diffs, each regulation directory has a notice directory
+        # with json files corrosponding to each other notice. 
+        diff_files = [os.path.join(dirname, f) for f in files]
+        regulation_files.extend(diff_files)
+
+    return regulation_files
+
+
+def send_to_s3(bucket, stub_base, path):
+    """
+    Send the JSON at the given `path` to the given Amazon S3 `bucket`.
+    Path components will be appended to the bucket and will match the
+    path beneith `stub_base`.
+    """
+    relative_path = os.path.relpath(path, stub_base)
+    print('sending {} to s3://{}/{}'.format(path, bucket.name, relative_path))
+
+    data = json.dumps(json.load(open(path, 'r')))
+
+    k = boto.s3.key.Key(bucket)
+    k.key = relative_path
+    k.set_contents_from_string(data)
+
+
+def send_to_server(api_base, stub_base, path):
+    """
+    Send the file at the given `path` to the given `api_base`. Path
+    components will be appended to the `api_base` and are presumed to
+    match.
+    """
+    relative_path = os.path.relpath(path, stub_base)
+    url = urlparse.urljoin(api_base, relative_path)
+
+    print('sending {} to {}'.format(path, url))
+
+    data = json.dumps(json.load(open(path, 'r')))
+    r = requests.post(url, data=data,
+                      headers={'content-type': 'application/json'})
+
+    # regulations-core returns 204 on a successful POST
+    if r.status_code != 204:
+        print("ERROR:", r.status_code, r.reason)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    # We can either send to the regulations-core API 
+    parser.add_argument('-a', '--api-base', action='store',
+            help='the regulations-core API URL')
+    # Or to an Amazon S3 bucket
+    parser.add_argument('-b', '--s3-bucket', action='store',
+            help='an S3 bucket name')
+
+    # We need to know where the JSON is coming from
+    parser.add_argument('-s', '--stub-base', action='store',
+            default=os.path.join(os.getcwd(), 'stub'), 
+            help='the base filesystem path for regulations JSON (default: ./stub)')
+
+    # We need to know what we're uploading, either a whole regulation's
+    # worth of JSON or a specific file or files
+    parser.add_argument('-r', '--regulation', nargs='?', action='store',
+            help='the specific regulation part number to upload (eg: 1026)')
+    parser.add_argument('-f', '--files', nargs='*', action='store',
+            default=[], help='specific JSON files to upload')
+
+    args = parser.parse_args()
+
+    # We need a destination
+    if args.api_base is None and args.s3_bucket is None:
+        print("ERROR: either -a api_base or -b s3_bucket is required.",
+                file=sys.stderr)
+        parser.print_help()
+        sys.exit(1)
+
+    # If we're going to upload all the JSON for a single regulation, we
+    # need to fetch those files.
+    if args.regulation is not None:
+        args.files = find_regulation_files(args.stub_base, args.regulation)
+
+    else:
+        print("ERROR: no file or regulation to upload. Use -r or -f to specify.",
+                file=sys.stderr)
+        parser.print_help()
+        sys.exit(1)
+
+    if args.api_base is not None:
+        [send_to_server(args.api_base, args.stub_base, f) 
+                for f in args.files]
+    elif args.s3_bucket is not None:
+        # Set up our S3 connection and get the bucket
+        try:
+            conn = boto.connect_s3()
+            bucket = conn.get_bucket(args.s3_bucket)
+        except boto.exception.S3ResponseError as e:
+            print("ERROR:", e)
+            sys.exit(1)
+
+        [send_to_s3(bucket, args.stub_base, f)
+                for f in args.files]
+
+ 
